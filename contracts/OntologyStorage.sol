682b3971821dc67147f21c28ff5b07b5dc9223d8
Maximilian Goisser <goisser94@gmail.com>
2018-06-26 16:18:38 +0200

    feat(lint): add solium as linter

--- contracts/OntologyStorage.sol
+++ contracts/OntologyStorage.sol
@@ -3,7 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import "./cid.sol";
 import "./pb_mod.sol";
-import './IPropositionStorage.sol';
+import "./IPropositionStorage.sol";
 
 /// @notice The OntologyStorage keeps track of all the ontological entities required to construct propositions, and the propositions themselves.
 // The structure for each of the entities is very similar:
@@ -17,225 +17,225 @@ import './IPropositionStorage.sol';
 // checkDependenciesAreStored<Entity> function: Check if all the CIDs that are referenced in the entity are already stored in the contract.
 // <Entity>Stored event: Is emitted when `store<Entity>` is called successfully.
 contract OntologyStorage is IPropositionStorage {
-  mapping (bytes32 => AnnotationCodec.Annotation) private annotations;
-  mapping (bytes32 => ClassCodec.Class) private classes;
-  mapping (bytes32 => IndividualCodec.Individual) private individuals;
+    mapping (bytes32 => AnnotationCodec.Annotation) private annotations;
+    mapping (bytes32 => ClassCodec.Class) private classes;
+    mapping (bytes32 => IndividualCodec.Individual) private individuals;
 
-  event AnnotationStored(bytes _cid);
-  event ClassStored(bytes _cid);
-  event IndividualStored(bytes _cid);
+    event AnnotationStored(bytes _cid);
+    event ClassStored(bytes _cid);
+    event IndividualStored(bytes _cid);
 
-  //
-  // Annotation
-  //
+    //
+    // Annotation
+    //
 
-  function storeAnnotation(bytes property, string value) public returns (bytes) {
-    var ann = AnnotationCodec.Annotation(property, value);
-    var hash = hashAnnotation(ann);
-    annotations[hash] = ann;
+    function storeAnnotation(bytes property, string value) public returns (bytes) {
+        var ann = AnnotationCodec.Annotation(property, value);
+        var hash = hashAnnotation(ann);
+        annotations[hash] = ann;
 
-    var annCid = cid.wrapInCid(cidPrefixAnnotation(), hash);
-    emit AnnotationStored(annCid);
-    return annCid;
-  }
-
-  function retrieveAnnotation(bytes annCid) public view returns (bytes, string) {
-    bytes32 hash = cid.unwrapCid(annCid);
-    var ann = annotations[hash];
-
-    return (ann.property, ann.value);
-  }
-
-  function isStoredAnnotation(bytes annCid) public view returns (bool exists) {
-    bytes32 hash = cid.unwrapCid(annCid);
-    AnnotationCodec.Annotation memory ann = annotations[hash];
-
-    bytes32 propertyHash = cid.unwrapCid(ann.property);
-    bytes32 defaultBytes;
-    return propertyHash != defaultBytes;
-  }
-
-  function calculateHashAnnotation(bytes property, string value) public view returns (bytes32) {
-    var ann = AnnotationCodec.Annotation(property, value);
-    return hashAnnotation(ann);
-  }
-
-  function calculateCidAnnotation(bytes property, string value) public view returns (bytes) {
-    var ann = AnnotationCodec.Annotation(property, value);
-    var hash = hashAnnotation(ann);
-    return cid.wrapInCid(cidPrefixAnnotation(), hash);
-  }
-
-  function cidPrefixAnnotation() private pure returns (bytes5) {
-    bytes5 annCidPrefix = 0x01f0011b20;
-    return annCidPrefix;
-  }
-
-  function hashAnnotation(AnnotationCodec.Annotation ann) private view returns (bytes32) {
-    var enc = AnnotationCodec.encode(ann);
-    var hash = keccak256(enc);
-
-    return hash;
-  }
-
-  //
-  // Class
-  //
-
-  function checkDependenciesAreStoredClass(ClassCodec.Class klass) internal view returns (bool) {
-    for (uint i = 0; i < klass.annotations.length; i++) {
-      var annotationCid = klass.annotations[i];
-      if (!isStoredAnnotation(annotationCid)) {
-        return false;
-      }
+        var annCid = cid.wrapInCid(cidPrefixAnnotation(), hash);
+        emit AnnotationStored(annCid);
+        return annCid;
     }
 
-    for (uint j = 0; j < klass.sub_class_of_class.length; j++) {
-      var classCid = klass.sub_class_of_class[j];
-      if (!isStoredClass(classCid)) {
-        return false;
-      }
+    function retrieveAnnotation(bytes annCid) public view returns (bytes, string) {
+        bytes32 hash = cid.unwrapCid(annCid);
+        var ann = annotations[hash];
+
+        return (ann.property, ann.value);
     }
 
-    return true;
-  }
+    function isStoredAnnotation(bytes annCid) public view returns (bool exists) {
+        bytes32 hash = cid.unwrapCid(annCid);
+        AnnotationCodec.Annotation memory ann = annotations[hash];
 
-  function storeClass(bytes[] _annotations, bytes[] sub_class_of_class) public returns (bytes) {
-    var klass = ClassCodec.Class(_annotations, sub_class_of_class);
-    require(checkDependenciesAreStoredClass(klass));
-    var hash = hashClass(klass);
-
-    classes[hash] = klass;
-
-    var classCid = cid.wrapInCid(cidPrefixClass(), hash);
-    emit ClassStored(classCid);
-    return classCid;
-  }
-
-  function retrieveClass(bytes classCid) public view returns (bytes[], bytes[]) {
-    bytes32 hash = cid.unwrapCid(classCid);
-    var klass = classes[hash];
-
-    return (klass.annotations, klass.sub_class_of_class);
-  }
-
-  function isStoredClass(bytes classCid) public view returns (bool exists) {
-    bytes32 hash = cid.unwrapCid(classCid);
-    ClassCodec.Class memory klass = classes[hash];
-
-    if (klass.annotations.length == 0 && klass.sub_class_of_class.length == 0) {
-      return false;
-    }
-    return true;
-  }
-
-  function calculateHashClass(bytes[] _annotations, bytes[] sub_class_of_class) public view returns (bytes32) {
-    var klass = ClassCodec.Class(_annotations, sub_class_of_class);
-    return hashClass(klass);
-  }
-
-  function calculateCidClass(bytes[] _annotations, bytes[] sub_class_of_class) public view returns (bytes _cid) {
-    var klass = ClassCodec.Class(_annotations, sub_class_of_class);
-    var hash = hashClass(klass);
-    return cid.wrapInCid(cidPrefixClass(), hash);
-  }
-
-  function cidPrefixClass() private pure returns (bytes5) {
-    bytes5 classCidPrefix = 0x01f1011b20;
-    return classCidPrefix;
-  }
-
-  function hashClass(ClassCodec.Class klass) private view returns (bytes32) {
-    var enc = ClassCodec.encode(klass);
-    var hash = keccak256(enc);
-
-    return hash;
-  }
-
-  //
-  // Individual
-  //
-  function checkDependenciesAreStoredIndividual(IndividualCodec.Individual ind) internal view returns (bool) {
-    for (uint i = 0; i < ind.annotations.length; i++) {
-      var annotationCid = ind.annotations[i];
-      if (!isStoredAnnotation(annotationCid)) {
-        return false;
-      }
+        bytes32 propertyHash = cid.unwrapCid(ann.property);
+        bytes32 defaultBytes;
+        return propertyHash != defaultBytes;
     }
 
-    for (uint j = 0; j < ind.class_assertions.length; j++) {
-      var classCid = ind.class_assertions[j];
-      if (!isStoredClass(classCid)) {
-        return false;
-      }
+    function calculateHashAnnotation(bytes property, string value) public view returns (bytes32) {
+        var ann = AnnotationCodec.Annotation(property, value);
+        return hashAnnotation(ann);
     }
 
-    for (uint k = 0; k < ind.negative_class_assertions.length; k++) {
-      var classCid2 = ind.negative_class_assertions[k];
-      if (!isStoredClass(classCid2)) {
-        return false;
-      }
+    function calculateCidAnnotation(bytes property, string value) public view returns (bytes) {
+        var ann = AnnotationCodec.Annotation(property, value);
+        var hash = hashAnnotation(ann);
+        return cid.wrapInCid(cidPrefixAnnotation(), hash);
     }
 
-    return true;
-  }
-
-  function storeIndividual(bytes[] _annotations, bytes[] _class_assertions, bytes[] _negative_class_assertions) public returns (bytes) {
-    var ind = IndividualCodec.Individual(_annotations, _class_assertions, _negative_class_assertions);
-    require(checkDependenciesAreStoredIndividual(ind));
-    var hash = hashIndividual(ind);
-    individuals[hash] = ind;
-
-    var indCid = cid.wrapInCid(cidPrefixIndividual(), hash);
-    emit IndividualStored(indCid);
-    return indCid;
-  }
-
-  function retrieveIndividual(bytes indCid) public view returns (bytes[], bytes[], bytes[]) {
-    bytes32 hash = cid.unwrapCid(indCid);
-    var ind = individuals[hash];
-
-    return (ind.annotations, ind.class_assertions, ind.negative_class_assertions);
-  }
-
-  function isStoredIndividual(bytes indCid) public view returns (bool exists) {
-    bytes32 hash = cid.unwrapCid(indCid);
-    IndividualCodec.Individual memory ind = individuals[hash];
-
-    if (ind.annotations.length == 0 && ind.class_assertions.length == 0) {
-      return false;
+    function cidPrefixAnnotation() private pure returns (bytes5) {
+        bytes5 annCidPrefix = 0x01f0011b20;
+        return annCidPrefix;
     }
-    return true;
-  }
 
-  function calculateHashIndividual(bytes[] _annotations, bytes[] _class_assertions, bytes[] _negative_class_assertions) public view returns (bytes32) {
-    var ind = IndividualCodec.Individual(_annotations, _class_assertions, _negative_class_assertions);
-    return hashIndividual(ind);
-  }
+    function hashAnnotation(AnnotationCodec.Annotation ann) private view returns (bytes32) {
+        var enc = AnnotationCodec.encode(ann);
+        var hash = keccak256(enc);
 
-  function calculateCidIndividual(bytes[] _annotations, bytes[] _class_assertions, bytes[] _negative_class_assertions) public view returns (bytes) {
-    var ind = IndividualCodec.Individual(_annotations, _class_assertions, _negative_class_assertions);
-    var hash = hashIndividual(ind);
-    return cid.wrapInCid(cidPrefixIndividual(), hash);
-  }
+        return hash;
+    }
 
-  function cidPrefixIndividual() private pure returns (bytes5) {
-    bytes5 cidPrefix = 0x01f2011b20;
-    return cidPrefix;
-  }
+    //
+    // Class
+    //
 
-  function hashIndividual(IndividualCodec.Individual ind) private view returns (bytes32) {
-    var enc = IndividualCodec.encode(ind);
-    var hash = keccak256(enc);
+    function checkDependenciesAreStoredClass(ClassCodec.Class klass) internal view returns (bool) {
+        for (uint i = 0; i < klass.annotations.length; i++) {
+            var annotationCid = klass.annotations[i];
+            if (!isStoredAnnotation(annotationCid)) {
+                return false;
+            }
+        }
 
-    return hash;
-  }
+        for (uint j = 0; j < klass.sub_class_of_class.length; j++) {
+            var classCid = klass.sub_class_of_class[j];
+            if (!isStoredClass(classCid)) {
+                return false;
+            }
+        }
 
-  //
-  // Interface: IPropositionStorage
-  //
+        return true;
+    }
 
-  function isPropositionStored(bytes proposition) public view returns (bool stored) {
-    return isStoredIndividual(proposition);
-  }
+    function storeClass(bytes[] _annotations, bytes[] sub_class_of_class) public returns (bytes) {
+        var klass = ClassCodec.Class(_annotations, sub_class_of_class);
+        require(checkDependenciesAreStoredClass(klass));
+        var hash = hashClass(klass);
+
+        classes[hash] = klass;
+
+        var classCid = cid.wrapInCid(cidPrefixClass(), hash);
+        emit ClassStored(classCid);
+        return classCid;
+    }
+
+    function retrieveClass(bytes classCid) public view returns (bytes[], bytes[]) {
+        bytes32 hash = cid.unwrapCid(classCid);
+        var klass = classes[hash];
+
+        return (klass.annotations, klass.sub_class_of_class);
+    }
+
+    function isStoredClass(bytes classCid) public view returns (bool exists) {
+        bytes32 hash = cid.unwrapCid(classCid);
+        ClassCodec.Class memory klass = classes[hash];
+
+        if (klass.annotations.length == 0 && klass.sub_class_of_class.length == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    function calculateHashClass(bytes[] _annotations, bytes[] sub_class_of_class) public view returns (bytes32) {
+        var klass = ClassCodec.Class(_annotations, sub_class_of_class);
+        return hashClass(klass);
+    }
+
+    function calculateCidClass(bytes[] _annotations, bytes[] sub_class_of_class) public view returns (bytes _cid) {
+        var klass = ClassCodec.Class(_annotations, sub_class_of_class);
+        var hash = hashClass(klass);
+        return cid.wrapInCid(cidPrefixClass(), hash);
+    }
+
+    function cidPrefixClass() private pure returns (bytes5) {
+        bytes5 classCidPrefix = 0x01f1011b20;
+        return classCidPrefix;
+    }
+
+    function hashClass(ClassCodec.Class klass) private view returns (bytes32) {
+        var enc = ClassCodec.encode(klass);
+        var hash = keccak256(enc);
+
+        return hash;
+    }
+
+    //
+    // Individual
+    //
+    function checkDependenciesAreStoredIndividual(IndividualCodec.Individual ind) internal view returns (bool) {
+        for (uint i = 0; i < ind.annotations.length; i++) {
+            var annotationCid = ind.annotations[i];
+            if (!isStoredAnnotation(annotationCid)) {
+                return false;
+            }
+        }
+
+        for (uint j = 0; j < ind.class_assertions.length; j++) {
+            var classCid = ind.class_assertions[j];
+            if (!isStoredClass(classCid)) {
+                return false;
+            }
+        }
+
+        for (uint k = 0; k < ind.negative_class_assertions.length; k++) {
+            var classCid2 = ind.negative_class_assertions[k];
+            if (!isStoredClass(classCid2)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function storeIndividual(bytes[] _annotations, bytes[] _class_assertions, bytes[] _negative_class_assertions) public returns (bytes) {
+        var ind = IndividualCodec.Individual(_annotations, _class_assertions, _negative_class_assertions);
+        require(checkDependenciesAreStoredIndividual(ind));
+        var hash = hashIndividual(ind);
+        individuals[hash] = ind;
+
+        var indCid = cid.wrapInCid(cidPrefixIndividual(), hash);
+        emit IndividualStored(indCid);
+        return indCid;
+    }
+
+    function retrieveIndividual(bytes indCid) public view returns (bytes[], bytes[], bytes[]) {
+        bytes32 hash = cid.unwrapCid(indCid);
+        var ind = individuals[hash];
+
+        return (ind.annotations, ind.class_assertions, ind.negative_class_assertions);
+    }
+
+    function isStoredIndividual(bytes indCid) public view returns (bool exists) {
+        bytes32 hash = cid.unwrapCid(indCid);
+        IndividualCodec.Individual memory ind = individuals[hash];
+
+        if (ind.annotations.length == 0 && ind.class_assertions.length == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    function calculateHashIndividual(bytes[] _annotations, bytes[] _class_assertions, bytes[] _negative_class_assertions) public view returns (bytes32) {
+        var ind = IndividualCodec.Individual(_annotations, _class_assertions, _negative_class_assertions);
+        return hashIndividual(ind);
+    }
+
+    function calculateCidIndividual(bytes[] _annotations, bytes[] _class_assertions, bytes[] _negative_class_assertions) public view returns (bytes) {
+        var ind = IndividualCodec.Individual(_annotations, _class_assertions, _negative_class_assertions);
+        var hash = hashIndividual(ind);
+        return cid.wrapInCid(cidPrefixIndividual(), hash);
+    }
+
+    function cidPrefixIndividual() private pure returns (bytes5) {
+        bytes5 cidPrefix = 0x01f2011b20;
+        return cidPrefix;
+    }
+
+    function hashIndividual(IndividualCodec.Individual ind) private view returns (bytes32) {
+        var enc = IndividualCodec.encode(ind);
+        var hash = keccak256(enc);
+
+        return hash;
+    }
+
+    //
+    // Interface: IPropositionStorage
+    //
+
+    function isPropositionStored(bytes proposition) public view returns (bool stored) {
+        return isStoredIndividual(proposition);
+    }
 }
--- contracts/RlayToken.sol
+++ contracts/RlayToken.sol
@@ -1,103 +1,103 @@
 pragma solidity ^0.4.19;
 
 import "tokens/eip20/EIP20.sol";
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
-import 'openzeppelin-solidity/contracts/MerkleProof.sol';
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/MerkleProof.sol";
 
 contract RlayToken is EIP20 {
-  // owner required for temporary workaround of permissioned payout calculation
-  address public owner;
-  // mapping of epochs to the merkle root of the payouts for that epoch
-  mapping (uint256 => bytes32) public payout_roots;
-  // keeps track of the total withrawals made for a address
-  mapping (address => uint256) public payout_withdrawals;
+    // owner required for temporary workaround of permissioned payout calculation
+    address public owner;
+    // mapping of epochs to the merkle root of the payouts for that epoch
+    mapping (uint256 => bytes32) public payout_roots;
+    // keeps track of the total withrawals made for a address
+    mapping (address => uint256) public payout_withdrawals;
 
-  function RlayToken() EIP20(
-    1000000000000000000000000000,
-    "RLAY Token",
-    18,
-    "RLAY"
-  ) public {
-     owner = msg.sender;
-  }
+    function RlayToken() EIP20(
+        1000000000000000000000000000,
+        "RLAY Token",
+        18,
+        "RLAY"
+    ) public {
+        owner = msg.sender;
+    }
 
-  function mintedAmountPerEpoch()
-    public
-    pure
+    function mintedAmountPerEpoch()
+        public
+        pure
     returns (uint256) {
-      return 25000000000000000000;
+        return 25000000000000000000;
     }
 
-  // Add a new payout root for a epoch
-  //
-  // Submiting a new payout merkle root currently works in a permissioned way,
-  // where only the token issuer can submit a new root.
-  // We intend to transition to a permissionless way to submit the payout root
-  // before launch of the mainnet, with zkSNARKs and Truebit being possible solutions.
-  function submitPayoutRoot(
-    uint256 _epoch,
-    bytes32 _root
-  )
-    public
+    // Add a new payout root for a epoch
+    //
+    // Submiting a new payout merkle root currently works in a permissioned way,
+    // where only the token issuer can submit a new root.
+    // We intend to transition to a permissionless way to submit the payout root
+    // before launch of the mainnet, with zkSNARKs and Truebit being possible solutions.
+    function submitPayoutRoot(
+        uint256 _epoch,
+        bytes32 _root
+    )
+        public
     {
-      require(msg.sender == owner);
-      payout_roots[_epoch] = _root;
+        require(msg.sender == owner);
+        payout_roots[_epoch] = _root;
 
-      uint256 _mintedAmount = mintedAmountPerEpoch();
-      totalSupply = SafeMath.add(totalSupply, _mintedAmount);
-      balances[this] = SafeMath.add(balances[this], _mintedAmount);
+        uint256 _mintedAmount = mintedAmountPerEpoch();
+        totalSupply = SafeMath.add(totalSupply, _mintedAmount);
+        balances[this] = SafeMath.add(balances[this], _mintedAmount);
 
-      emit Transfer(address(0), this, _mintedAmount);
+        emit Transfer(address(0), this, _mintedAmount);
     }
 
-  function redeemPayout(
-    uint256 _epoch,
-    bytes32[] _proof,
-    address _account,
-    uint256 _amount // Accumulated amount at that payout epoch
-  )
-    public
+    function redeemPayout(
+        uint256 _epoch,
+        bytes32[] _proof,
+        address _account,
+        uint256 _amount // Accumulated amount at that payout epoch
+    )
+        public
     {
-      require(verifyPayout(_epoch, _proof, _account, _amount));
+        require(verifyPayout(_epoch, _proof, _account, _amount));
 
-      // Calculate the remaining eligable payout for the account (subtract previous withrawals)
-      uint256 _withdrawals_for_account = payout_withdrawals[_account];
-      uint256 _withdrawal_amount = SafeMath.sub(_amount, _withdrawals_for_account);
-      require(_withdrawal_amount > 0, "Not possible to withdraw a negative or zero amount.");
+        // Calculate the remaining eligable payout for the account (subtract previous withrawals)
+        uint256 _withdrawals_for_account = payout_withdrawals[_account];
+        uint256 _withdrawal_amount = SafeMath.sub(_amount, _withdrawals_for_account);
+        require(_withdrawal_amount > 0, "Not possible to withdraw a negative or zero amount.");
 
-      // Adjust balances and withdrawal tracker
-      balances[this] = SafeMath.sub(balances[this], _withdrawal_amount);
-      balances[_account] = SafeMath.add(balances[_account], _withdrawal_amount);
-      payout_withdrawals[_account] = SafeMath.add(payout_withdrawals[_account], _withdrawal_amount);
-      emit Transfer(this, _account, _withdrawal_amount);
+        // Adjust balances and withdrawal tracker
+        balances[this] = SafeMath.sub(balances[this], _withdrawal_amount);
+        balances[_account] = SafeMath.add(balances[_account], _withdrawal_amount);
+        payout_withdrawals[_account] = SafeMath.add(payout_withdrawals[_account], _withdrawal_amount);
+        emit Transfer(this, _account, _withdrawal_amount);
     }
 
-  function verifyPayout(
-    uint256 _epoch,
-    bytes32[] _proof,
-    address _account,
-    uint256 _amount // Accumulated amount at that payout epoch
-  )
-    public
-    view
-    returns (bool)
+    function verifyPayout(
+        uint256 _epoch,
+        bytes32[] _proof,
+        address _account,
+        uint256 _amount // Accumulated amount at that payout epoch
+    )
+        public
+        view
+        returns (bool)
     {
-      // Construct leaf hash and verify proof
-      bytes memory _leaf_data = abi.encodePacked(_account, _amount);
-      bytes32 _leaf = keccak256(keccak256(_leaf_data));
-      bytes32 _epoch_root = payout_roots[_epoch];
-      return verifyProof(_proof, _epoch_root, _leaf);
+        // Construct leaf hash and verify proof
+        bytes memory _leaf_data = abi.encodePacked(_account, _amount);
+        bytes32 _leaf = keccak256(keccak256(_leaf_data));
+        bytes32 _epoch_root = payout_roots[_epoch];
+        return verifyProof(_proof, _epoch_root, _leaf);
     }
 
-  function verifyProof(
-    bytes32[] _proof,
-    bytes32 _root,
-    bytes32 _leaf
-  )
-    public
-    pure
-    returns (bool)
+    function verifyProof(
+        bytes32[] _proof,
+        bytes32 _root,
+        bytes32 _leaf
+    )
+        public
+        pure
+        returns (bool)
     {
-      return MerkleProof.verifyProof( _proof, _root, _leaf);
-  }
+        return MerkleProof.verifyProof(_proof, _root, _leaf);
+    }
 }
