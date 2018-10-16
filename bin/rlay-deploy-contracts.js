bb3cdf708112b07ccbf473879fc4123c3ad22def
Maximilian Goisser <goisser94@gmail.com>
2018-10-16 19:29:43 +0200

    fix(deploy): fix deploy binary

--- bin/rlay-deploy-contracts.js
+++ bin/rlay-deploy-contracts.js
@@ -3,15 +3,23 @@
 const child_process = require("child_process");
 const path = require("path");
 
-const stdout = child_process.execSync(
-  "node_modules/.bin/truffle migrate --reset",
-  {
-    cwd: path.resolve(process.cwd(), "./node_modules/@rlay/protocol"),
-    env: Object.assign({}, process.env, {
-      NODE_OPTIONS: "--max-old-space-size=120000"
-    })
-  }
+const stdoutInstall = child_process.execSync(`npm install`, {
+  cwd: path.resolve(process.cwd(), "./node_modules/@rlay/protocol"),
+  env: process.env
+});
+const outputInstallStr = stdoutInstall.toString("utf8");
+console.log(outputInstallStr);
+
+const trufflePath = path.resolve(
+  process.cwd(),
+  "./node_modules/@rlay/protocol/node_modules/.bin/truffle"
 );
+const stdout = child_process.execSync(`${trufflePath} migrate --reset`, {
+  cwd: path.resolve(process.cwd(), "./node_modules/@rlay/protocol"),
+  env: Object.assign({}, process.env, {
+    NODE_OPTIONS: "--max-old-space-size=120000"
+  })
+});
 
 const outputStr = stdout.toString("utf8");
 console.log(outputStr);
